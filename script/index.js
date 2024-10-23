346f7212304bda2400bd19bebb1963fe64d6e669
VictorHenrique01 <vic10souz@gmail.com>
2024-10-22 22:09:10 -0300

    Botões(por enquanto sem funcionalidade) e ID foram add na tabela

--- script/index.js
+++ script/index.js
@@ -1,5 +1,4 @@
 async function listarEnderecos() {
-
     const url = "https://go-wash-api.onrender.com/api/auth/address";
     let token = localStorage.getItem('access_token');
 
@@ -12,43 +11,42 @@ async function listarEnderecos() {
         let api = await fetch(url, {
             method: "GET",
             headers: {
-                'Authorization': `Bearer ${token}`  
+                'Authorization': `Bearer ${token}`
             }
         });
 
         if (api.ok) {
             let resposta = await api.json();
-            console.log(resposta); 
+            console.log(resposta);
             
-            let enderecos = resposta.data; 
-
-            let tbody = document.querySelector("tbody");
+            let enderecos = resposta.data;
+            let tbody = document.getElementById("addressTableBody");
             tbody.innerHTML = ""; 
 
-            
             if (Array.isArray(enderecos)) {
                 enderecos.forEach(endereco => {
                     let row = document.createElement("tr");
                     row.innerHTML = `
+                        <td>${endereco.id}</td>
                         <td>${endereco.title}</td>
                         <td>${endereco.cep}</td>
                         <td>${endereco.address}</td>
                         <td>${endereco.number}</td>
+                        <td><button class="botao_atualiza" data-id="${endereco.id}">Atualizar</button></td>
+                        <td><button class="botao_deleta" data-id="${endereco.id}">Deletar</button></td>
                     `;
                     tbody.appendChild(row);
                 });
             } else {
-                console.error("enderecos não é um array:", enderecos);
+                console.error("enderecos não são um array:", enderecos);
                 alert("Não foram encontrados endereços.");
             }
         } else {
             let respostaErro = await api.json();
             console.error(respostaErro);
-            alert(respostaErro.message || "Erro ao listar endereços."); 
         }
     } catch (error) {
         console.error("Erro ao listar endereços:", error);
-        alert("Erro ao listar endereços.");
     }
 }
 
